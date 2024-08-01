0ed7d45bd351425000167c81443702a741f92171
ShoxruxAsadov <shoxaasadov2005@gmail.com>
2024-08-01 15:08:43 +0500

    CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app/app.module';
 
 async function nest() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: true });
-  await app.setGlobalPrefix("api").listen(process.env.PORT);
+  app.enableCors({ origin: '*' });
+  await app.setGlobalPrefix('api').listen(process.env.PORT);
 }
 nest();
