e1555541e6e0c822342c33b1cc373084abf630d4
ShoxruxAsadov <shoxaasadov2005@gmail.com>
2024-08-01 15:16:50 +0500

    listen server

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,6 @@ import { AppModule } from './app/app.module';
 async function nest() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
-  await app.setGlobalPrefix('api').listen(process.env.PORT);
+  await app.setGlobalPrefix('api').listen(process.env.PORT || 8080);
 }
 nest();
