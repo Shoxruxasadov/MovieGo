cf5f0204523e424b377af3f9c9b954cb4850d92d
ShoxruxAsadov <shoxaasadov2005@gmail.com>
2024-09-26 23:58:08 +0500

    Fixed Error

--- client/src/components/movie/related.js
+++ client/src/components/movie/related.js
@@ -3,6 +3,7 @@ import { useRouter } from 'next/router';
 import Link from "next/link"
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import translate from "@/language/translate.json"
+import { useStore } from '@/store/zustand';
 
 export default function MovieRelated() {
   const getRelated = useStore(state => state.getRelated);
