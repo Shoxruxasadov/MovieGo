de4786401b0d2bbd2343a779bb0f08aff66b0fb9
ShoxruxAsadov <shoxaasadov2005@gmail.com>
2025-01-05 04:00:54 +0500

    Done!

--- server/src/studios/studios.module.ts
+++ server/src/studios/studios.module.ts
@@ -4,11 +4,13 @@ import { StudiosController } from './studios.controller';
 import { Movies, MoviesSchema } from 'src/movies/movies.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Studios, StudiosSchema } from './studios.schema';
+import { Famous, FamousSchema } from 'src/famous/famous.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Studios.name, schema: StudiosSchema }]),
     MongooseModule.forFeature([{ name: Movies.name, schema: MoviesSchema }]),
+    MongooseModule.forFeature([{ name: Famous.name, schema: FamousSchema }]),
   ],
   controllers: [StudiosController],
   providers: [StudiosService],
